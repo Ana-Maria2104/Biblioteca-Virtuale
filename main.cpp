7657d23646b9338da7f9b5331c908cdaa6a034ec
Ana Maria Draghici <anamaria.draghici@studenti.unipd.it>
2025-07-23 09:26:21 +0200

    Add files Project pt1

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include <QApplication>
+#include <QStackedWidget>
+#include <QFileDialog>
+#include <QMessageBox>
+#include "gui/MainWindow.h"
+#include "gui/UserPage.h"
+#include "gui/AdminPage.h"
+#include "jsonHandler/Json.h"
+#include "gui/EditCreatePage.h"
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+    QStackedWidget stackWidget;
+
+    // Selezione file JSON
+    Json jManager;  // Oggetto automatico, non più puntatore
+    QString path = QFileDialog::getOpenFileName(nullptr, "Scegli il file .json da cui prendere i dati",
+                                                "", "Json (*.json)");
+
+    if (path.isEmpty()) {
+        QMessageBox::critical(nullptr, "Errore",
+                              "Nessun file selezionato. L'applicazione verrà chiusa.");
+        return EXIT_FAILURE;
+    }
+    jManager.setFilePath(path);
+
+    // Caricamento dati
+    QList<BibliotecaItem *> listaOggetti = jManager.loadList();
+
+    // Creazione delle pagine
+    MainWindow *mainWindow = new MainWindow(&stackWidget);
+    UserPage *userPage = new UserPage(listaOggetti, &stackWidget);
+    AdminPage *adminPage = new AdminPage(listaOggetti, &stackWidget);
+    EditCreatePage *editCreatePage = new EditCreatePage(&stackWidget, adminPage);
+
+    // Connessioni per salvare prenotazioni e restituzioni
+    QObject::connect(userPage, &UserPage::itemUpdated, &jManager, &Json::aggiornaStatoOggetto);
+    QObject::connect(adminPage, &AdminPage::itemUpdated, &jManager, &Json::aggiornaStatoOggetto);
+    QObject::connect(adminPage, &AdminPage::itemRemoved, &jManager, &Json::rimuoviOggetto);
+    QObject::connect(adminPage, &AdminPage::createItem, editCreatePage, &EditCreatePage::setupForCreation);
+    QObject::connect(adminPage, &AdminPage::modifyItem, editCreatePage, &EditCreatePage::setupForEditing);
+
+    QObject::connect(editCreatePage, &EditCreatePage::itemCreated, &jManager, &Json::aggiungiOggetto);
+    QObject::connect(editCreatePage, &EditCreatePage::itemUpdated, &jManager, &Json::modificaOggetto);
+
+    // Observers
+    jManager.aggiungiObserver(userPage);
+    jManager.aggiungiObserver(adminPage);
+
+    // Aggiunta delle pagine
+    stackWidget.addWidget(mainWindow);      // index 0
+    stackWidget.addWidget(userPage);        // index 1
+    stackWidget.addWidget(adminPage);       // index 2
+    stackWidget.addWidget(editCreatePage);  // index 3
+    stackWidget.show();
+
+    return app.exec();
+}
+
